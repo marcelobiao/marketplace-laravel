7e17f8a94064f593b5425efeeb261e9139acf0dd
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-25 16:17:02 -0300

    Ajustando rotas de stores

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->namespace('Admin')->group(function(){
+
+    Route::prefix('stores')->group(function(){
+
+        Route::get('/', 'StoreController@index');
+        Route::get('/create', 'StoreController@create');
+        Route::post('/store', 'StoreController@store');
+
+    });
+
+});
