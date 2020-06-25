f8415bd6ed542dedad601e52068fbcf9f5f11dd9
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-25 16:17:45 -0300

    Criando controller de store

---
+++ app/Http/Controllers/Admin/StoreController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    public function index(){
+        $stores = \App\Store::paginate(10);
+
+        return view('admin.stores.index', compact('stores'));
+    }
+
+    public function create(){
+        $users = \App\User::all(['id', 'name']);
+        return view('admin.stores.create', compact('users'));
+    }
+
+    public function store(Request $req){
+        $data = $req->all();
+
+        $user= \App\User::find($data['user']);
+
+        $store = $user->store()->create($data);
+
+        return $store;
+    }
+
+}
