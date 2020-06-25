9d7dd7bc0003e6d88c1d6cb4128b28a095512721
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-25 16:18:06 -0300

    Criando view index de stores

---
+++ resources/views/admin/stores/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<table class='table table-stried'>
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Loja</th>
+            <th>Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($stores as $store)
+            <tr>
+                <td>{{$store->id}}</td>
+                <td>{{$store->name}}</td>
+                <td></td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
+{{$stores->links()}}
+@endSection
