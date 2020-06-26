416b62b9d2c1201483d93dddd6c3f16e8e14d653
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-26 14:01:26 -0300

    Ajustando rotas da aplicação

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function(){
 
-    Route::prefix('stores')->name('stores.')->group(function(){
+    /* Route::prefix('stores')->name('stores.')->group(function(){
 
         Route::get('/', 'StoreController@index')->name('index');
         Route::get('/create', 'StoreController@create')->name('create');
@@ -26,6 +26,9 @@ Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function(){
         Route::post('/update/{store}', 'StoreController@update')->name('update');
         Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
 
-    });
+    }); */
+
+    Route::resource('stores', 'StoreController');
+    Route::resource('products', 'ProductController');
 
 });
