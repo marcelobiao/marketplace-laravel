e3ce34a490f3a92530c779867ab3118b607a589e
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-27 12:57:48 -0300

    Ajustando views de Products para utilizar ProductRequest

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,42 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Criar loja</h1>
+    <h1>Criar Produto</h1>
     <form action="{{route('admin.products.store')}}" method="post">
         @csrf
 
         <div class='form-group'>
             <label for="">Nome Produto</label>
-            <input type="text" name="name" class='form-control'>
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{old('name')}}">
+            @error('name')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
         </div>
 
         <div class='form-group'>
             <label for="">Descrição</label>
-            <input type="text" name="description" class='form-control'>
+            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror" value="{{old('description')}}">
+            @error('description')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
         </div>
 
         <div class='form-group'>
             <label for="">Conteúdo</label>
-            <textarea name="body" id="" cols="30" rows="10" class="form-control"></textarea>
+            <textarea name="body" id="" cols="30" rows="10" class="form-control @error('body') is-invalid @enderror" value="{{old('body')}}"></textarea>
+            @error('body')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
         </div>
 
         <div class='form-group'>
             <label for="">Preço</label>
-            <input type="text" name="price" class='form-control'>
+            <input type="text" name="price" class="form-control @error('price') is-invalid @enderror" value="{{old('price')}}">
+            @error('price')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
         </div>
 
         <div class='form-group'>
             <label for="">Slug</label>
-            <input type="text" name="slug" class='form-control'>
-        </div>
-
-        <div class='form-group'>
-            <label for="">Lojas</label>
-            <select name="store" class='form-control'>
-                @foreach($stores as $store)
-                    <option value="{{$store->id}}">{{$store->name}}</option>
-                @endforeach
-            </select>
+            <input type="text" name="slug" class="form-control">
         </div>
 
         <div>
